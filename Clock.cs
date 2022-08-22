126b491bc61907048d4b77940953519397c079eb
adnanxvii <75388080+adnanxvii@users.noreply.github.com>
2022-08-22 19:13:49 +1000

    Add files via upload

---
+++ Clock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Clock
+{
+    public class Clock
+    {
+        private Counter _seconds;
+        private Counter _minutes;
+        private Counter _hours;
+
+        public Clock()
+        {
+            _seconds = new Counter("seconds");
+            _minutes = new Counter("minutes");
+            _hours = new Counter("hours");
+        }
+
+        public void Tick()
+        {
+            _seconds.Increment();
+            if (_seconds.Ticks == 60)
+            {
+                _minutes.Increment();
+                _seconds.Reset();
+            }
+
+            if (_minutes.Ticks == 60)
+            {
+                _hours.Increment();
+                _minutes.Reset();
+            }
+
+            if (_hours.Ticks == 24)
+            {
+                _hours.Reset();
+            }
+
+        }
+
+        public void Reset()
+        {
+            _seconds.Reset();
+            _minutes.Reset();
+            _hours.Reset();
+        }
+
+        public string Time
+        {
+            get
+            {
+                string s = _hours.Ticks.ToString("D2") + ":" + _minutes.Ticks.ToString("D2") + ":" + _seconds.Ticks.ToString("D2");
+                return s;
+            }
+        }
+    }
+}
