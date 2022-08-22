99370668440db1bcf56ea7e9f987cd319796d694
adnanxvii <75388080+adnanxvii@users.noreply.github.com>
2022-08-22 19:12:49 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Clock
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+
+
+            for (int i = 0; i < 9050; i++) //2hours30minutes50seconds
+            {
+                myClock.Tick();
+            }
+
+
+            Console.WriteLine(myClock.Time);
+
+
+            myClock.Reset(); //back to 00:00:00
+            Console.WriteLine(myClock.Time);
+
+
+            myClock.Tick(); // ticks once to 00:00:01
+            Console.WriteLine(myClock.Time);
+
+        }
+    }
+}
